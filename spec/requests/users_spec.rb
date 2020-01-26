c9f41deb19e6050b8f804b83d7cb2cd2739f84ad
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-01-26 21:55:09 +0900

    Use SSL and the Puma webserver in production

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,36 @@ RSpec.describe 'access to users', type: :request do
       expect(response).to have_http_status 200
     end
   end
+
+  describe 'POST #create' do
+    context 'valid request' do
+      it 'adds a user' do
+        expect do
+          post signup_path, params: { user: attributes_for(:user) }
+        end.to change(User, :count).by(1)
+      end
+
+      context 'adds a user' do
+        before { post signup_path, params: { user: attributes_for(:user) } }
+        subject { response }
+        it { is_expected.to redirect_to user_path(User.last) } #showページにリダイレクトされる
+        it { is_expected.to have_http_status 302 } #リダイレクト成功
+      end
+    end
+
+    context 'invalid request' do
+      let(:user_params) do
+        attributes_for(:user, name: '',
+                              email: 'user@invalid',
+                              password: '',
+                              password_confirmation: '')
+      end
+
+      it 'does not add a user' do
+        expect do
+          post signup_path, params: { user: user_params }
+        end.to change(User, :count).by(0)
+      end
+    end
+  end
 end
