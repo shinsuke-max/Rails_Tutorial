06d1e12fdfe36560c3d282d0d1d54961c0253a6b
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-02-06 14:33:58 +0900

    Micropost search_form実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,14 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      if params[:q] && params[:q].reject { |key, value| value.blank? }.present?
+        @q = current_user.feed.ransack(microposts_search_params)
+        @feed_items = @q.result.paginate(page: params[:page])
+      else
+        @q = Micropost.none.ransack
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      end
+      @url = root_path
     end
   end
 
@@ -14,5 +21,11 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+
+  private
+  
+    def microposts_search_params
+      params.require(:q).permit(:content_cont)
+    end
   
 end
